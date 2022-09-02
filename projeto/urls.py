cbc0ec53580914ed6f033135556660854af4abe4
ramon s antonio <monsillva@gmail.com>
2022-09-02 13:39:50 -0300

    url alterada

--- projeto/urls.py
+++ projeto/urls.py
@@ -13,20 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from http.client import HTTPResponse
-from pathlib import Path
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path
-
-def home(request):
-    return HttpResponse('home')
-
-def contato(request):
-    return HttpResponse('contato')
-
-def sobre(request):
-    return HttpResponse('sobre')
+from recipes.views import home, contato, sobre
 
 
 urlpatterns = [
