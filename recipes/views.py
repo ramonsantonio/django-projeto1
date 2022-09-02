faaf0563da31eb09e955a0802400982db08fc47a
ramon s antonio <monsillva@gmail.com>
2022-09-02 13:04:03 -0300

    Alteração de httResponse

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('home')
+
+def contato(request):
+    return HttpResponse('contato')
+
+def sobre(request):
+    return HttpResponse('sobre')
